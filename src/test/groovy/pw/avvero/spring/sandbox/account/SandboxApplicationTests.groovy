4a0c3219d15a3585fa51dbf7ee551775a8e72c52
belyaev-ay <anton.belyaev@sibit.tech>
2023-12-01 13:01:03 +0700

    Spock and groovy for testing

---
+++ src/test/groovy/pw/avvero/spring/sandbox/account/SandboxApplicationTests.groovy
@@ -0,0 +1,19 @@
+package pw.avvero.spring.sandbox.account
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import pw.avvero.spring.sandbox.ContainersConfiguration
+import spock.lang.Specification
+
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
+@ContextConfiguration(classes = [ContainersConfiguration])
+class SandboxApplicationTests extends Specification {
+
+    def "Context loads"() {
+        expect:
+        1 == 1
+    }
+
+}
