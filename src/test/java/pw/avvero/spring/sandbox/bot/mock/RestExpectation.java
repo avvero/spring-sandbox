64d7f49ceb37853ddd71f2a7c17ed45589937072
belyaev-ay <anton.belyaev@sibit.tech>
2024-07-02 12:44:31 +0700

    Improve RestExpectation

--- src/test/java/pw/avvero/spring/sandbox/bot/mock/RestExpectation.java
+++ src/test/java/pw/avvero/spring/sandbox/bot/mock/RestExpectation.java
@@ -1,30 +1,44 @@
 package pw.avvero.spring.sandbox.bot.mock;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.ResponseCreator;
-import org.springframework.test.web.client.response.DefaultResponseCreator;
 import org.springframework.web.client.RestTemplate;
 import pw.avvero.test.http.RequestCaptor;
 
-public class RestExpectation extends RestExpectationMockRestServiceServer {
+import static org.springframework.test.web.client.ExpectedCount.manyTimes;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 
+public class RestExpectation {
+
+    private final MockRestServiceServer mockServer;
     public final OpenaiMock openai;
     public final TelegramMock telegram;
 
     public RestExpectation(RestTemplate restTemplate) {
-        super(MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build());
+        mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build();
         this.openai = new OpenaiMock() {
             @Override
             public RequestCaptor completions(ResponseCreator responseCreator) {
-                return map("https://api.openai.com/v1/chat/completions", responseCreator);
+                return map(mockServer, "https://api.openai.com/v1/chat/completions", responseCreator);
             }
         };
         this.telegram = new TelegramMock() {
             @Override
             public RequestCaptor sendMessage(ResponseCreator responseCreator) {
-                return map("https://api.telegram.org/sendMessage", responseCreator);
+                return map(mockServer, "https://api.telegram.org/sendMessage", responseCreator);
             }
         };
+    }
+
+    protected RequestCaptor map(MockRestServiceServer mockServer, String uri, ResponseCreator responseCreator) {
+        RequestCaptor requestCaptor = new RequestCaptor();
+        mockServer.expect(manyTimes(), requestTo(uri))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(requestCaptor)
+                .andRespond(responseCreator);
+        return requestCaptor;
     }
 
     public void cleanup() {
